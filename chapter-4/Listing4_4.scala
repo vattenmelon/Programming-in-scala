c65a731fb8b48b34ebacb922e1d1f80b29f0347a
Erling Reizer <reizer@gmail.com>
2013-12-22 13:38:12 +0100

    .

--- chapter-4/Listing4_4.scala
+++ chapter-4/Listing4_4.scala
@@ -1,5 +1,6 @@
 import ChecksumAccumulator.calculate
 
+//Application is deprecated, use App instead says the 2.10 compiler. book is for 2.8
 object FallWinterSpringSummer extends App {
 	for (season <- List("fall", "winter", "spring"))
 		println(season + ": " + calculate(season))
