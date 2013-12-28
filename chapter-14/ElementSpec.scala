feef00c4da98e10695866c9ccc43e12c1aa7e8ac
Erling Reizer <reizer@gmail.com>
2013-12-28 20:01:45 +0100

    .

--- chapter-14/ElementSpec.scala
+++ chapter-14/ElementSpec.scala
@@ -2,19 +2,18 @@ import org.scalatest._
 import Element.elem
 
 class ElementSpec extends FlatSpec with Matchers {
+  
+  val ele = elem('x', 2, 3)
 
   "An element" should "have a width" in {
-    val ele = elem('x', 2, 3)
     ele.width should be (2)
   }
 
   it should "have a height" in {
-    val ele = elem('x', 2, 3)
     ele.height should be (3)
   }
 
   it should "have a toString" in {
-    val ele = elem('x', 2, 3)
     ele.toString should not be (null) 
   }
 }
