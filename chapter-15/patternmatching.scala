0158905561fed04e3675f30a8ad927cc1d8f2c77
Erling Reizer <reizer@gmail.com>
2013-12-29 13:42:21 +0100

    .

--- chapter-15/patternmatching.scala
+++ chapter-15/patternmatching.scala
@@ -9,3 +9,11 @@ println(metode(6))
 println(metode(9))
 println(metode(10))
 
+
+def tupleDemo(expr: Any) =
+  expr match {
+    case (a, b, c) => println("matched " + a + b + c)
+    case _ =>
+  }
+
+tupleDemo(("a", 3, "-tuple"))
