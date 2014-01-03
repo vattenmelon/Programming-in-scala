57cd58a4f8e96768a378c69aa9fc2754fd6bda05
Erling Reizer <reizer@gmail.com>
2014-01-03 20:30:41 +0100

    .

---
+++ chapter-17/Sets.scala
@@ -0,0 +1,9 @@
+import scala.collection.mutable
+
+val text = "See Spot run. Run, Spot. Run!"
+val wordsArray = text.split("[ !,.]+")
+val words = mutable.Set.empty[String]
+for (word <- wordsArray)
+  words += word.toLowerCase
+
+println(words)
